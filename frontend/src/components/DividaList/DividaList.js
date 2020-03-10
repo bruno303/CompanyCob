87f47ae85a5dbb066aa29dc425a22c2256b63bc8
Bruno Oliveira <brunooliveira812@gmail.com>
2020-03-09 21:48:10 -0300

    exibindo dados da divida original na tela de divida calculada

--- frontend/src/components/DividaList/DividaList.js
+++ frontend/src/components/DividaList/DividaList.js
@@ -6,7 +6,16 @@ import api from '../../service/api';
 class DividaList extends Component {
     dataDevedor = {};
 
-    state = { parcelas: [] };
+    state = {
+        dividaCalculada: {
+            divida: {
+                valorOriginal: 0,
+                valorJuros: 0,
+                valorFinal: 0
+            }
+        },
+        parcelas: []
+    };
 
     constructor(props) {
         super(props);
@@ -38,11 +47,11 @@ class DividaList extends Component {
 
     async getDividaCalculada(idDevedor, idDivida) {
         try {
-            const data = await api.get(`/v1/calculo/${idDevedor}/${idDivida}`)
-            if (data.status === 200) {
-                this.setState({ parcelas: data.data.parcelas });
+            const result = await api.get(`/v1/calculo/${idDevedor}/${idDivida}`)
+            if (result.status === 200) {
+                this.setState({ dividaCalculada: result.data.data, parcelas: result.data.data.parcelas });
             } else {
-                this.showError(`Erro ao consultar as parcelas calculadas. Status ${data.status}`);
+                this.showError(`Erro ao consultar as parcelas calculadas. Status ${result.status}`);
             }
         } catch (err) {
             this.showError(`Erro ao consultar as parcelas calculadas: ${err}`);
@@ -79,10 +88,23 @@ class DividaList extends Component {
         return result;
     }
 
+    renderDetalhesDividaCalculada() {
+        return (
+            <div className="row">
+                <div>R$ {this.state.dividaCalculada.divida.valorOriginal.toFixed(2)}</div>
+                <div>{new Date(this.state.dividaCalculada.divida.vencimento).toLocaleDateString()}</div>
+                <div>{this.state.dividaCalculada.divida.diasAtraso}</div>
+                <div>{this.state.dividaCalculada.qtdParcelas}</div>
+                <div>{this.state.dividaCalculada.divida.valorJuros.toFixed(2)}</div>
+                <div>{this.state.dividaCalculada.divida.valorFinal.toFixed(2)}</div>
+            </div>
+        );
+    }
+
     renderDividas() {
         let result = [];
 
-        for (let index = 0; index < this.props.data.dividas.length; index++) {
+        for (let index = 0; index < this.dataDevedor.dividas.length; index++) {
 
             const idDivida = this.dataDevedor.dividas[index].id;
             const vencimento = new Date(this.dataDevedor.dividas[index].vencimento);
@@ -116,14 +138,30 @@ class DividaList extends Component {
                 <div id="modal-result" className="modal">
                     <div className="modal-content">
                         <span className="close">&times;</span>
+
+
+                        <div className="details-results-div">
+                            <div className="row header-font-result">
+                                <div>Valor Original</div>
+                                <div>Vencimento Original</div>
+                                <div>Dias de Atraso</div>
+                                <div>Quantidade de Parcelas</div>
+                                <div>Valor Juros</div>
+                                <div>Valor Final</div>
+                            </div>
+                            {this.renderDetalhesDividaCalculada()}
+                        </div>
+                        <hr />
                         <div id="results-div" className="results-div">
-                            <div className="row header-font">
+                            <div className="row header-font-result">
                                 <div>Parcela</div>
                                 <div>Valor</div>
                                 <div>Vencimento</div>
                             </div>
                             {this.renderDividaCalculada()}
                         </div>
+                        <hr />
+                        <span>Para negociar com as melhores condições, entre em contato pelo telefone {this.state.dividaCalculada.telefoneContato}</span>
 
                     </div>
                 </div>
